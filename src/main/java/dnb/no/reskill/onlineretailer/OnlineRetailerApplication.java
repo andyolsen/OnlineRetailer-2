f1277a148fa9e6c6e9b07ac7d45aede595e77021
Knut Maurset <knut.espen.maurset@dnb.no>
2021-03-15 13:54:08 +0100

    ds

---
+++ src/main/java/dnb/no/reskill/onlineretailer/OnlineRetailerApplication.java
@@ -0,0 +1,14 @@
+package dnb.no.reskill.onlineretailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRetailerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineRetailerApplication.class, args);
+    }
+
+    
+}
---
+++ src/test/java/dnb/no/reskill/onlineretailer/OnlineRetailerApplicationTests.java
@@ -0,0 +1,13 @@
+package dnb.no.reskill.onlineretailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRetailerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
