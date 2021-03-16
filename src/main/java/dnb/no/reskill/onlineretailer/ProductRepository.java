6efda577076b56eabb322a9889b8bf74e061992e
Andy Olsen <andyo@olsensoft.com>
2021-03-16 07:13:18 +0000

    Update ProductRepository.java

--- src/main/java/dnb/no/reskill/onlineretailer/ProductRepository.java
+++ src/main/java/dnb/no/reskill/onlineretailer/ProductRepository.java
@@ -4,7 +4,9 @@ import java.util.Collection;
 
 public interface ProductRepository {
 
+        // FROM ANDY: The id isn't known until after insertion. So this method should return the id (or whole Product object).
         void addProduct(Product product);
+        
         boolean deleteProduct(int productId);
         boolean updateProduct(Product product);
         Product findProduct(int productId);
